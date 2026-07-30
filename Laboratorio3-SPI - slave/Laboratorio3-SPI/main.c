e85a299c33efa38c36f023ce63894d5b5a6eed87
Aquiej12 <abnerquiej12@gmail.com>
2026-07-30 17:37:08 -0600

    s

--- Laboratorio3-SPI - slave/Laboratorio3-SPI/main.c
+++ Laboratorio3-SPI - slave/Laboratorio3-SPI/main.c
@@ -1,8 +1,7 @@
 /*
  * Laboratorio3-SPI.c   (ESCLAVO)
  *
- * Created: 23/07/2026 15:37:30
- * Author : abner
+ * Autor: Abner Quiej y Edwin Parada
  */
 
 #define F_CPU 16000000UL
@@ -12,11 +11,13 @@
 #include <stdint.h>
 #include "SPI.h"
 
-volatile uint8_t valorUART = 0;
+
+volatile uint8_t valorLEDs = 0; 
 volatile uint8_t pot1 = 0;
 volatile uint8_t pot2 = 0;
+volatile uint8_t flag_ignorar_dummy = 0; 
 
-void UARTout(void);
+void mostrarEnLeds(uint8_t valor);
 void initADC(void);
 unsigned int ADC_Read(char canal);
 
@@ -24,60 +25,75 @@ int main(void)
 {
     spiInit(SPI_SLAVE_SS, SPI_DATA_ORDER_MSB, SPI_CLOCK_IDLE_LOW, SPI_CLOCK_FIRST_EDGE);
     initADC();
-    SPCR |= (1<<SPIE); // Habilitar interrupciones SPI
-    sei(); // Habilitar interrupciones globales
-	// Iniciar portD como salida
-	DDRD = 0xFF;
-	// PORTD incialmente apagado
-	PORTD = 0x00;
-	//deshabilitar rx y tx
-	UCSR0B &= ~((1<<RXEN0)|(1<<TXEN0));
-
+    SPCR |= (1<<SPIE); 
+    sei(); 
+	
+    // Deshabilitar RX y TX 
+    UCSR0B &= ~((1<<RXEN0)|(1<<TXEN0));
+    // Puerto D como salidas
+    DDRD = 0xFF; 
+    mostrarEnLeds(0);
+    
     while (1)
     {
-        pot1 = ADC_Read(0) >> 2; // 8 bits altos del potenciometro en ADC0
-        pot2 = ADC_Read(1) >> 2; // 8 bits altos del potenciometro en ADC1
+        // Leer ADC 
+        pot1 = ADC_Read(0) >> 2; 
+        pot2 = ADC_Read(1) >> 2; 
+        
         _delay_ms(100);
-		UARTout();
+        
+        // Actualizar los LEDs
+        mostrarEnLeds(valorLEDs);
     }
 }
 
-void UARTout(void)
+
+void mostrarEnLeds(uint8_t valor)
 {
-	
-	// Cargar valor de UART a PORTD
-	PORTD = valorUART;
+    PORTD = valor;
 }
 
 void initADC(void)
 {
+
     ADMUX  = (1 << REFS0);
+    // Habilitar ADC, Prescaler de 128
     ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-    }
-
-unsigned int ADC_Read(char canal){
-	ADMUX = (ADMUX & 0xF0) | (canal & 0x0F);
-	_delay_us(10);
-
-	ADCSRA |= (1 << ADSC);
-	while(ADCSRA & (1 << ADSC));
-
-	return ADC;
 }
 
+unsigned int ADC_Read(char canal)
+{
+    // Seleccionar el canal ADC
+    ADMUX = (ADMUX & 0xF0) | (canal & 0x0F);
+    _delay_us(10);
+    ADCSRA |= (1 << ADSC);
+    while(ADCSRA & (1 << ADSC));
 
-// El maestro envia 'c' para pedir pot1 y 'd' para pedir pot2, si no es ninguno de los dos:
-// Guardar lo recibido en valorUART
+    return ADC;
+}
+
+// INTERRUPCIÓN SPI 
+
 ISR(SPI_STC_vect)
 {
-    uint8_t spiValor = SPDR;
+    uint8_t spiValor = SPDR; 
+    
+    // Si es dummy
+    if (flag_ignorar_dummy == 1) {
+        flag_ignorar_dummy = 0; 
+        return; /
+    }
+    
+    // Si no es dummy
     if(spiValor == 'c'){
-        SPDR = pot1;
+        SPDR = pot1;            
+        flag_ignorar_dummy = 1; 
     }
     else if(spiValor == 'd'){
-        SPDR = pot2;
+        SPDR = pot2;            
+        flag_ignorar_dummy = 1; 
     }
-	
-	valorUART = spiValor;
-	
+    else {
+        valorLEDs = spiValor;
+    }
 }
